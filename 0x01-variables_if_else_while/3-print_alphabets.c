014d7515f4ff003f415cb8ace090a7fb8a8d7ffd
ekeminiakpabio <ekeminiakpabio44@gmail.com>
2022-10-16 13:21:06 -0700

    commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * Return: 0
+ */
+int main(void)
+{
+	char ls;
+
+	for (ls = 'a'; ls <= 'z'; ls++)
+		putchar(ls);
+	for (ls = 'A'; ls <= 'Z'; ls++)
+		putchar(ls);
+
+	putchar(\n);
+	return (0);
+}
