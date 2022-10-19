57b6d42f1ab741c05d57b6587ae3afad2e7e402c
ekeminiakpabio <ekeminiakpabio44@gmail.com>
2022-10-19 14:31:55 -0700

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
  * print_alphabet - prints salphabets in lc
  */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char lc;
 
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,7 @@
  * void print_alphabet_x10 - 10 times
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 	int ten;
 	char lc;
