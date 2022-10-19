16d0bb4ec2fc7ec2206df5edf67834d0a464ac4b
ekeminiakpabio <ekeminiakpabio44@gmail.com>
2022-10-19 16:46:02 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @r: interger to be cump
+ * Return: absolute value
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (-r);
+}
