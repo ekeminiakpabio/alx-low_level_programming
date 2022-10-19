3329da9af0410a0a27e1b803cb3b724b8ec5d80c
ekeminiakpabio <ekeminiakpabio44@gmail.com>
2022-10-19 14:58:00 -0700

    commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - lower than
+ * @c: character
+ * Return: 0 or 1 depends on return value
+ *
+ */
+
+int _islower(int c)
+{
+	if (c => 'a'; && c<= 'z')
+		return (1);
+
+	else
+		return (0);
+}
