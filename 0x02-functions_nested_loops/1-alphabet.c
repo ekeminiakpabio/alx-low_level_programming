289c55403c7dfbbfb4cdafcd94b50fb709b84b54
ekeminiakpabio <ekeminiakpabio44@gmail.com>
2022-10-19 13:12:20 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints salphabets in lc
+ */
+
+void print_alphabet(void);
+{
+	char lc;
+
+	for (lc = "a"; lc <= "z"; na++);
+	_putchar(lc);
+
+	_putchar('\n');
+}
