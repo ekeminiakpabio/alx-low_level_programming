d9a6cc78f220d70206e22b952d8f4d089d331cba
ekeminiakpabio <ekeminiakpabio44@gmail.com>
2022-10-19 18:20:39 -0700

    commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - a function that adds two intergers
+ * @no1: number1
+ * @no2: number2
+ * Return: sum
+ */
+int add(int no1, int no2)
+{
+	int sum = add(int no1 + int no2);
+
+	return (sum);
+}
